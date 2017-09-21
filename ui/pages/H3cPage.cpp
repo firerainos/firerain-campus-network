9f3e709467d4086ca22759cec4a42d76372ea854
1377195627 <1377195627@qq.com>
2017-09-21 15:03:33 +0800

    完成华三认证基本UI

--- ui/pages/H3cPage.cpp
+++ ui/pages/H3cPage.cpp
@@ -8,17 +8,50 @@
 
 H3cPage::H3cPage(QWidget *parent) :
     QFrame(parent){
-    QFormLayout *mainLayout = new QFormLayout(this);
 
-    QLabel *h3cLogo = new QLabel(this);
-    h3cLogo->setPixmap(QPixmap(":/images/h3c.ico"));
+    auto *mainLayout = new QVBoxLayout(this);
+
+    this->setLayout(mainLayout);
+
+    auto *h3cLogo = new QLabel(this);
+    h3cLogo->setFixedSize(100, 100);
+    h3cLogo->setPixmap(QPixmap(":/images/h3c.ico").scaled(100,100));
+
+    auto *usernameLabel = new QLabel("用户名:", this);
+    auto *passwdLabel = new QLabel("密 码:", this);
+
+    usernameLabel->setFixedWidth(80);
+    passwdLabel->setFixedWidth(80);
 
     usernameEdit = new QLineEdit(this);
     passwdEdit = new QLineEdit(this);
 
-    startButton = new QPushButton("连接",this);
+    usernameEdit->setFixedWidth(200);
+    passwdEdit->setFixedWidth(200);
 
-    mainLayout->addRow("用户名", usernameEdit);
-    mainLayout->addRow("密码", passwdEdit);
-    mainLayout->addRow(startButton);
+    startButton = new QPushButton("连接",this);
+    startButton->setFixedSize(150, 35);
+
+    auto *usernameLayout = new QHBoxLayout(this);
+    auto *passwdLayout = new QHBoxLayout(this);
+
+    usernameLayout->addStretch();
+    usernameLayout->addWidget(usernameLabel);
+    usernameLayout->addWidget(usernameEdit);
+    usernameLayout->addStretch();
+
+    passwdLayout->addStretch();
+    passwdLayout->addWidget(passwdLabel);
+    passwdLayout->addWidget(passwdEdit);
+    passwdLayout->addStretch();
+
+    mainLayout->addStretch();
+    mainLayout->addWidget(h3cLogo,0,Qt::AlignCenter);
+    mainLayout->addSpacing(50);
+    mainLayout->addLayout(usernameLayout);
+    mainLayout->addLayout(passwdLayout);
+    mainLayout->addSpacing(20);
+    mainLayout->addWidget(startButton,0,Qt::AlignCenter);
+    mainLayout->addStretch();
+
 }
